6eef892b0c038c60f0a8b071697468aaa33af9e1
SuPeskov <supeskov@yandex.ru>
2025-01-26 14:15:05 +0300

    создана страница доступа

---
+++ start.js
@@ -0,0 +1,33 @@
+const productIP = document.querySelector('#inpProduct');
+const productPass = document.querySelector('#inpPass');
+const productSubmit = document.querySelector('#log');
+
+async function login() {
+  try {
+      const res = await fetch("https://c02584c6ba10fa08.mokky.dev/auth", {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            email: productIP.value,
+            password: productPass.value,
+          })
+        });
+
+        const data = await res.json()
+        localStorage.setItem('token', data.token);
+        console.log(data);
+
+    window.location.href = 'index.html'
+
+  } catch(error) {
+      console.error(`Ошибка ${error}`);
+  };
+}
+
+productSubmit.addEventListener('click', (event) => {
+  event.preventDefault();
+  login();
+});
