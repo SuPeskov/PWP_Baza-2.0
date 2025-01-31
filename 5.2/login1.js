55eb5edd54ee71c6270e9ca7edef7e462cf60da2
SuPeskov <supeskov@yandex.ru>
2025-01-31 15:04:47 +0300

    обновлена ссылка перхода к шапке

--- 5.2/login1.js
+++ 5.2/login1.js
@@ -1,5 +1,5 @@
 const token = sessionStorage.getItem('token');
 
 if ((!token) || (token == 'undefined')) {
-    window.location.href = '../start.html';
+    window.location.href = 'https://supeskov.github.io/LogInSystem/';
 }
