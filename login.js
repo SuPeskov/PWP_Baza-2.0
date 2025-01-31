29b4765d7b44c36cbc80d4047b6985419790f846
SuPeskov <supeskov@yandex.ru>
2025-01-31 12:45:08 +0300

    скорректирван залогиненый вход в систему

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 const token = sessionStorage.getItem('token');
 
 if ((!token) || (token == 'undefined')) {
-    window.location.href = './start.html';
+    window.location.href = 'https://supeskov.github.io/PWP_Baza-2.0/5.2/5.2.4.html';
 }
