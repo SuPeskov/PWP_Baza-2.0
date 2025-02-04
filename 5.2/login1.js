a79fd830d2e9e33e52c0557ca5b6d765e216e323
SuPeskov <supeskov@yandex.ru>
2025-02-04 15:51:28 +0300

    обновлена ссылка в скрипте логина, удалены лишние скрипты

--- 5.2/login1.js
+++
@@ -1,5 +0,0 @@
-const token = sessionStorage.getItem('token');
-
-if ((!token) || (token == 'undefined')) {
-    window.location.href = 'https://supeskov.github.io/LogInSystem/';
-}
--- 5.3/login1.js
+++
@@ -1,5 +0,0 @@
-const token = sessionStorage.getItem('token');
-
-if ((!token) || (token == 'undefined')) {
-    window.location.href = '../start.html';
-}
